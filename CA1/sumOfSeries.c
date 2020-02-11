e40de4416ad6c3c3053e0011c74eabbc993b5c50
scriptkidee99 <ashvin.kasture12345@gmail.com>
2020-02-11 08:40:49 +0530

    fixed the program of the sum of the series

--- CA1/sumOfSeries.c
+++ CA1/sumOfSeries.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int power(int, int);
 void main(){
-    int n,i;
+    int n,i,sum=0;
     printf("Enter n to print the sum of the series (1+2^2+3^3+4^4) : ");
     scanf("%d",&n);
     printf("\nThe series is ");
-    for(i=1;i<=n;i++) printf(" %d ",power(i,i));
+    for(i=1;i<=n;i++) sum += power(i,i);
+    printf("\nThe sum of the series is %d",sum);
 }
 int power(int base, int pow){
     int i,result=1;
